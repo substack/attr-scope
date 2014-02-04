417a2e24e4d8754f8a64e25cabf2ac469fdb06ac
James Halliday <mail@substack.net>
2014-02-04 03:09:45 -0800

    example

---
+++ example/browser.js
@@ -0,0 +1,18 @@
+var observe = require('observable');
+var attractor = require('attractor');
+var scope = { robot: Robot };
+
+function Robot (elem) {
+    this.counter = observe(0);
+}
+
+Robot.prototype.vote = function () {
+    this.counter(this.counter() + 1);
+};
+
+var attr = attractor({
+    'x-scope': require('../'),
+    'x-bind': require('attr-bind'),
+    'x-click': [ require('attr-ev'), 'click' ]
+}, scope);
+attr.scan(document);
